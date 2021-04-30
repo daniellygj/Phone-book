2fff4379e691a66da367e32e8ef4c39dcaef6980
Danielly <danielly.jardim@triasoftware.com>
2021-04-30 16:11:55 -0300

    editar contatos

--- src/main/java/com/phonebook/service/ContactService.java
+++ src/main/java/com/phonebook/service/ContactService.java
@@ -4,6 +4,7 @@ import com.phonebook.controller.ContactFilter;
 import com.phonebook.model.Contact;
 import com.phonebook.repository.ContactFilterQuery;
 import com.phonebook.repository.ContactRepository;
+import com.phonebook.utils.exception.Exception;
 import com.phonebook.utils.exception.Exception.EmptyPhonesException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -29,5 +30,20 @@ public class ContactService {
 
 	public void deleteContact(Long id) {
 		repository.deleteById(id);
+	}
+
+	public Contact editContact(Contact contactEdited) {
+		if (contactEdited.getPhone() == null || contactEdited.getPhone().isEmpty()) {
+			throw new Exception.EmptyPhonesException();
+		}
+
+		Contact contactSaved = repository.getOne(contactEdited.getId());
+
+		contactSaved.setEmail(contactEdited.getEmail());
+		contactSaved.setFirstName(contactEdited.getFirstName());
+		contactSaved.setLastName(contactEdited.getLastName());
+		contactSaved.setPhone(contactEdited.getPhone());
+
+		return repository.save(contactSaved);
 	}
 }
--- src/test/java/com/phonebook/service/ContactServiceTest.java
+++ src/test/java/com/phonebook/service/ContactServiceTest.java
@@ -5,6 +5,7 @@ import com.phonebook.model.ContactTestBuilder;
 import com.phonebook.model.Phone;
 import com.phonebook.model.PhoneTestbuilder;
 import com.phonebook.repository.ContactRepository;
+import com.phonebook.utils.exception.Exception;
 import com.phonebook.utils.exception.Exception.EmptyPhonesException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +13,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -73,5 +76,78 @@ public class ContactServiceTest {
 		doNothing().when(repository).deleteById(CONTACT_ID);
 		service.deleteContact(CONTACT_ID);
 		verify(repository).deleteById(CONTACT_ID);
+	}
+
+	@Test
+	public void editContactShouldSucceed() {
+		Phone phone1 = PhoneTestbuilder
+				.init()
+				.withDefaultValues()
+				.id(1L)
+				.build();
+
+		Phone phone2 = PhoneTestbuilder
+				.init()
+				.withDefaultValues()
+				.id(2L)
+				.build();
+
+		Phone phone3 = PhoneTestbuilder
+				.init()
+				.withDefaultValues()
+				.id(3L)
+				.build();
+
+		Phone phone2Edited = PhoneTestbuilder
+				.init()
+				.withDefaultValues()
+				.id(2L)
+				.number("11 11111111")
+				.build();
+
+		List<Phone> phonesSaved = Arrays.asList(phone1, phone2, phone3);
+		List<Phone> phonesEdited = Arrays.asList(phone1, phone2Edited, phone3);
+
+		Contact contactSaved = ContactTestBuilder
+				.init()
+				.withDefaultValues()
+				.id(CONTACT_ID)
+				.phone(phonesSaved)
+				.build();
+
+		Contact contactEdited = ContactTestBuilder
+				.init()
+				.withDefaultValues()
+				.id(CONTACT_ID)
+				.firstName("Edited")
+				.firstName("Edited")
+				.email("edited@hotmail.com")
+				.phone(phonesEdited)
+				.build();
+
+		when(repository.getOne(contactEdited.getId())).thenReturn(contactSaved);
+		when(repository.save(contactSaved)).thenReturn(contactEdited);
+
+		Contact contact = service.editContact(contactEdited);
+
+		assertEquals(contactEdited.getId(), contact.getId());
+		assertEquals(contactEdited.getLastName(), contact.getLastName());
+		assertEquals(contactEdited.getFirstName(), contact.getFirstName());
+		assertEquals(contactEdited.getPhone(), contact.getPhone());
+		assertEquals(contactEdited.getEmail(), contact.getEmail());
+	}
+
+	@Test(expected = Exception.EmptyPhonesException.class)
+	public void editContactShouldFail() {
+		Contact contactEdited = ContactTestBuilder
+				.init()
+				.withDefaultValues()
+				.id(CONTACT_ID)
+				.firstName("Edited")
+				.firstName("Edited")
+				.email("edited@hotmail.com")
+				.build();
+
+		service.editContact(contactEdited);
 	}
 }
